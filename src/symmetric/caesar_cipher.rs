531c46f6c8bf04f9fe0c9f6fbe93665cdd39d809
Wesley-Arizio <wesleyarizio26@gmail.com>
2024-01-07 14:37:04 -0300

    refactor caesar cipher implementation

--- src/symmetric/caesar_cipher.rs
+++ src/symmetric/caesar_cipher.rs
@@ -1,31 +1,18 @@
-const ALPHABET: &[char] = &[
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-];
-
-pub fn encrypt(plain_text: &str, secret_key: usize) -> String {
+pub fn encrypt(plain_text: &str, secret_key: u8) -> String {
     let mut cipher_text = String::new();
     for c in plain_text.to_uppercase().chars() {
-        if let Some(index) = ALPHABET.iter().position(|v| *v == c) {
-            let new_index = (index + secret_key) % ALPHABET.len();
-            cipher_text.push_str(&ALPHABET[new_index].to_string());
-        } else {
-            cipher_text.push_str(" ");
-        }
+        let ascii_code = (c as u8 + secret_key) % u8::MAX;
+        cipher_text.push_str(char::from(ascii_code).to_string().as_ref());
     }
 
     cipher_text
 }
 
-pub fn decrypt(cipher_text: &str, secret_key: usize) -> String {
+pub fn decrypt(cipher_text: &str, secret_key: u8) -> String {
     let mut plain_text = String::new();
     for c in cipher_text.to_uppercase().chars() {
-        if let Some(index) = ALPHABET.iter().position(|v| *v == c) {
-            let new_index = (index - secret_key) % ALPHABET.len();
-            plain_text.push_str(&ALPHABET[new_index].to_string());
-        } else {
-            plain_text.push_str(" ");
-        }
+        let ascii_code = (c as u8 - secret_key) % u8::MAX;
+        plain_text.push_str(char::from(ascii_code).to_string().as_ref());
     }
 
     plain_text
@@ -39,12 +26,12 @@ mod test {
         let message = "Hello World";
         let secret = 3;
         let result = encrypt(&message, secret);
-        assert_eq!(result, "KHOOR ZRUOG");
+        assert_eq!(result, "KHOOR#ZRUOG");
     }
 
     #[test]
     fn decrypt_message() {
-        let cipher_text = "KHOOR ZRUOG";
+        let cipher_text = "KHOOR#ZRUOG";
         let secret = 3;
         let result = decrypt(&cipher_text, secret);
         assert_eq!(result, "HELLO WORLD");
