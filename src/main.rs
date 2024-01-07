80582f747c1cb97cf4eed54041d383c0c7dbcfa2
Wesley-Arizio <wesleyarizio26@gmail.com>
2024-01-07 14:59:03 -0300

    brute force attack in ceasar cipher decrypt method

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod symmetric;
 
 fn main() {
     // TODO - configure CLI command with args so that we can run our cryptography algorithms
+    // TODO - configure our main app to create a new file with the algorithm output
 }
--- src/symmetric/caesar_cipher.rs
+++ src/symmetric/caesar_cipher.rs
@@ -1,7 +1,7 @@
 pub fn encrypt(plain_text: &str, secret_key: u8) -> String {
     let mut cipher_text = String::new();
     for c in plain_text.to_uppercase().chars() {
-        let ascii_code = (c as u8 + secret_key) % u8::MAX;
+        let ascii_code = (c as u8).saturating_add(secret_key) % u8::MAX;
         cipher_text.push_str(char::from(ascii_code).to_string().as_ref());
     }
 
@@ -11,11 +11,21 @@ pub fn encrypt(plain_text: &str, secret_key: u8) -> String {
 pub fn decrypt(cipher_text: &str, secret_key: u8) -> String {
     let mut plain_text = String::new();
     for c in cipher_text.to_uppercase().chars() {
-        let ascii_code = (c as u8 - secret_key) % u8::MAX;
+        let ascii_code = (c as u8).saturating_sub(secret_key) % u8::MAX;
         plain_text.push_str(char::from(ascii_code).to_string().as_ref());
     }
 
     plain_text
+}
+
+pub fn brute_force_attack(cipher_text: &str) -> Vec<String> {
+    let mut result = vec![];
+
+    for i in 0..u8::MAX {
+        result.push(decrypt(cipher_text, i));
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -35,5 +45,22 @@ mod test {
         let secret = 3;
         let result = decrypt(&cipher_text, secret);
         assert_eq!(result, "HELLO WORLD");
+    }
+
+    #[test]
+    fn brute_force_attack_encrypted_message() {
+        let message = "KHOOR#ZRUOG";
+        let decrypted = brute_force_attack(message);
+
+        for i in 0..u8::MAX {
+            match i {
+                3 => {
+                    assert_eq!(decrypted[i as usize], "HELLO WORLD");
+                }
+                _ => {
+                    assert_ne!(decrypted[i as usize], "HELLO WORLD");
+                }
+            }
+        }
     }
 }
